fb66268d4acc8ee8f6708c1ca5076115bc7b8306
me1stplease <arman.khan152@gmail.com>
2024-02-22 09:57:49 +0530

    first functioning

--- DGenUI.py
+++ DGenUI.py
@@ -1,21 +1,115 @@
 import streamlit as st
-from PropComp import Properties as prop
+import pandas as pd
+import csv
+from Transpose import transpose as tps
+from numpy.lib.recfunctions import drop_fields
+
+from DataGen import Name as nm
+from DataGen import Phone as ph
+from DataGen import Email as em
 
 global datatype
 global columnName
-with st.container():
+global property1
+global property2
+global property3
+global property4
+
+
+records = 10
+
+# f = open("input.csv", "w")
+# f.write("datatype,columnName,property1,property2,property3,property4\n")
+# f.close()
+
+with st.container(border=True):
     col1, col2, col3 = st.columns(3)
 
     with col1:
         datatype = st.selectbox(
             'Data Type: ',
             ('Name', 'Phone', 'Email'))
-        st.write(datatype)
 
     with col2:
         columnName = st.text_input('Attribute Name: ', 'Text')
-        st.write(columnName)
 
     with col3:
-        prop(st, datatype)
+        if datatype == "Name":
+            ntype = st.selectbox(
+                'Type: ',
+                ('Full Name', 'First', 'Last'))
+            property1 = ntype
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        if datatype == "Phone":
+            plen = ''
+            property1 = plen
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+        else:
+            property1 = ''
+            property2 = ''
+            property3 = ''
+            property4 = ''
+
+col1, col2 = st.columns(2)
+
+with col1:
+    if st.button("Add Attribute", type="primary"):
+        f = open("input.csv", "a")
+        f.write(
+            datatype + "," + columnName + "," + property1 + "," + property2 + "," + property3 + "," + property4 + "\n")
+        f.close()
+        st.success("Attribute has been added")
+
+with col2:
+    if st.button("Clear All", type="secondary"):
+        f = open("input.csv", "w")
+        f.write("datatype,columnName,property1,property2,property3,property4\n")
+        f.close()
+
+st.divider()
+
+if st.button("Generate Data", type="primary"):
+    global data
+    with open('input.csv', 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+
+    output_record = []
+
+    print(len(data))
+
+    for i in range(1, len(data)):
+        if data[i][0] == "Name":
+            name = nm(records, data[i][0])
+            output_record.append(name)
+
+        if data[i][0] == "Phone":
+            phone = ph(records)
+            output_record.append(phone)
+
+        if data[i][0] == "Email":
+            email = em(records)
+            output_record.append(email)
+
+    print("final_op:")
+    print(output_record)
+    Tp_output = [['' for x in range(len(output_record))] for y in range(len(output_record[0]))]
+
+    tps(output_record, Tp_output, len(output_record), len(output_record[0]))
+
+    print("Modified matrix is")
+    print(Tp_output)
+
+    myFile = open('output.csv', 'w')
+    writer = csv.writer(myFile)
+
+    for data_list in Tp_output:
+        writer.writerow(data_list)
+    myFile.close()
 
--- DataGen.py
+++ DataGen.py
@@ -1,13 +1,34 @@
-def Name(ntype):
-    return "name"
+from faker import Faker
+
+fake = Faker(['en_US'])
 
 
-def Phone(len=10):
-    return "P"
+def Name(records, ntype):
+    name = []
+    for i in range(records):
+        if ntype == 'Full Name':
+            name.append(fake.name())
+        elif ntype == 'First':
+            name.append(fake.name().split(" ")[0])
+        else:
+            name.append(fake.name().split(" ")[1])
+
+    return name
 
 
-def Email(ext='@gmail.com'):
-    return "E"
+def Phone(records):
+    phone = []
+    for i in range(records):
+        phone.append(fake.unique.numerify('##########'))
+    return phone
+
+
+def Email(records):
+    email = []
+    for i in range(records):
+        nm = fake.name().split(" ")
+        email.append(nm[0] + fake.unique.numerify('###') + "@" + fake.domain_name())
+    return email
 
 
 def Amount(min=0, max=9999):
